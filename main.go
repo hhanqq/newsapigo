6706302140959403f4d23b67af54dd9039e95550
hhanqq <sdudnik05@mail.ru>
2024-04-25 17:26:01 +0700

    Добавление разметки в index.html и шаблона в main.go

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package newsapigo
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+// строки - срез байтов
+func indexHandler(w http.ResponseWriter, r *http.Request) { // Обработчик для HTTP-запросов на путь "/"
+	tpl.Execute(w, nil)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	//ServeMux - сопостовление url  с обработчиком запросов
+	mux := http.NewServeMux() // новый маршрутизатор
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
